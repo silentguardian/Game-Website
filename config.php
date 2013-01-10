8a35262657ea5a52357db02005b677815e975410
silentguardian <basmimarsinan@hotmail.com>
2013-01-10 18:40:37 +0100

    ! Cleaning up some stuff that got in accidentally.

--- config.php
+++ config.php
@@ -18,7 +18,7 @@ $core['cookie'] = 'gw1998';
 $core['api_pass'] = '';
 $core['clean_url'] = false;
 
-$core['site_url'] = 'http://localhost/game/';
+$core['site_url'] = '';
 $core['site_dir'] = dirname(__FILE__);
 
 $core['root_dir'] = $core['site_dir'] . '/library';
@@ -27,7 +27,7 @@ $core['modules_dir'] = $core['root_dir'] . '/modules';
 
 $db = array();
 
-$db['server'] = 'localhost';
-$db['name'] = 'game';
-$db['user'] = 'root';
+$db['server'] = '';
+$db['name'] = '';
+$db['user'] = '';
 $db['password'] = '';
--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 10, 2013 at 03:25 PM
+-- Generation Time: Jan 10, 2013 at 06:36 PM
 -- Server version: 5.5.16
 -- PHP Version: 5.3.8
 
@@ -35,16 +35,6 @@ CREATE TABLE IF NOT EXISTS `comment` (
   PRIMARY KEY (`id_comment`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Dumping data for table `comment`
---
-
-INSERT INTO `comment` (`id_comment`, `id_game`, `id_user`, `body`, `created`) VALUES
-(1, 3, 1, 'A nice game!', 1357569141),
-(3, 3, 1, 'What did you say?!', 1357569872),
-(5, 5, 1, 'Oh! Who created this game?!', 1357570354),
-(6, 3, 1, 'Where did no comments go?!', 1357570844);
-
 -- --------------------------------------------------------
 
 --
@@ -59,25 +49,6 @@ CREATE TABLE IF NOT EXISTS `customize` (
   `value` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   PRIMARY KEY (`id_customize`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `customize`
---
-
-INSERT INTO `customize` (`id_customize`, `id_game`, `id_level`, `id_item`, `value`) VALUES
-(1, 3, 1, 1, 'cool code!'),
-(2, 3, 1, 5, 'another cool code!'),
-(3, 3, 1, 9, 'very cool code!'),
-(4, 3, 5, 1, 'this is the coolest'),
-(5, 3, 5, 2, 'you'),
-(6, 3, 5, 3, 'can'),
-(7, 3, 5, 4, 'get'),
-(8, 3, 5, 5, 'when'),
-(9, 3, 5, 6, 'you'),
-(10, 3, 5, 7, 'try'),
-(11, 3, 5, 8, 'that'),
-(12, 3, 5, 9, 'really'),
-(13, 3, 5, 10, 'serious');
 
 -- --------------------------------------------------------
 
@@ -96,15 +67,6 @@ CREATE TABLE IF NOT EXISTS `game` (
   PRIMARY KEY (`id_game`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Dumping data for table `game`
---
-
-INSERT INTO `game` (`id_game`, `name`, `description`, `id_user`, `created`, `played`, `comments`) VALUES
-(2, 'Nice Game', 'Very very very nice game!', 1, 0, 251, 0),
-(3, 'Bad Game', 'Very very very bad game!', 1, 0, 2, 3),
-(5, 'Sample Game', 'Super cool description!\r\n\r\nYou wanted this! :D', 1, 1357478053, 1, 1);
-
 -- --------------------------------------------------------
 
 --
@@ -118,7 +80,7 @@ CREATE TABLE IF NOT EXISTS `score` (
   `time` int(10) NOT NULL DEFAULT '0',
   `value` smallint(5) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_score`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -136,15 +98,6 @@ CREATE TABLE IF NOT EXISTS `user` (
   `admin` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_user`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id_user`, `id_unique`, `username`, `password`, `email_address`, `registered`, `admin`) VALUES
-(1, 'cd9cd682ab', 'sinan', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', 'test@test.com', 1357408612, 0),
-(2, '29cd447b04', 'test', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', 'test2@test.com', 1357827100, 0),
-(3, '9f6a79bfcd', 'test2', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', 'test2a@test.com', 1357827307, 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
